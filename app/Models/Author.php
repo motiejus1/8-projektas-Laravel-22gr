a72b9b8e0b1c72e326b9cd7815e17740e737b5e0
ingabesus <93394843+ingabesus@users.noreply.github.com>
2022-02-19 10:15:05 +0200

    Update Author.php
    -delete

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-use Kyslik\ColumnSortable\Sortable;
+
 
 class Author extends Model
 {
