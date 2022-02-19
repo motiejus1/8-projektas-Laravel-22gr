440ad0019cd85182eafd0aa692f8a91ef54ea17b
ingabesus <93394843+ingabesus@users.noreply.github.com>
2022-02-19 10:16:32 +0200

    *test

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -6,11 +6,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 
-
 class Author extends Model
 {
     use HasFactory;
     public function authorBooks() {
         return $this->hasMany(Book::class, 'author_id', 'id');
+    }
+
+    public function test() {
+        return 0;
     }
 }
